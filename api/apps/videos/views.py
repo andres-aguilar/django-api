b27e2bf2f262eea12dcee296e17a53d003ee981e
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-23 10:01:47 -0600

    Permitiendo registrar nuevos videos mediante POST

--- api/apps/videos/views.py
+++ api/apps/videos/views.py
@@ -12,3 +12,13 @@ class ListVideos(APIView):
     def get(self, request):
         videos = VideoSerializer(Video.objects.all(), many=True)
         return Response(videos.data)
+
+    def post(self, request):
+        video = VideoSerializer(data=request.data)
+
+        if video.is_valid():
+            video.save()
+            return Response(video.data, status=201)
+        
+        return Response(video.errors, status=400)
+            
