c62572eb56c81c146a646c31fc198de27b23942e
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-23 09:53:39 -0600

    Registrando rest_framework en el archivo settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # REST Framework
+    'rest_framework',
     # Local apps
     'apps.videos'
 ]
