0ad5d6ac461091a81f7cbc6b776ab78763a416bd
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-24 12:16:38 -0600

    Generando relación con el mdelo User y Agregandolo a la respuesta de detalle

--- api/apps/videos/serializers.py
+++ api/apps/videos/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from .models import Video
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
 class VideoSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Video
-        fields = ('id', 'title', 'description', 'slug')
+        fields = ('id', 'title', 'description', 'slug', 'user')
